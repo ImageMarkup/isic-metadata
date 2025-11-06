bac267412f59a313f95065b1248aaf5d2b104bd9
Dan LaManna <dan.lamanna@kitware.com>
2025-11-06 12:58:52 -0500

    Verify the schema can be exported to pyarrow format

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,6 @@
+from pydantic_to_pyarrow import get_pyarrow_schema
+
+from isic_metadata.metadata import MetadataRow
 from isic_metadata.utils import get_unstructured_columns
 
 
@@ -7,3 +10,7 @@ def test_get_unstructured_columns():
 
 def test_get_unstructured_columns_ignore_filename():
     assert get_unstructured_columns(["age", "foo", "bar", "filename"]) == ["bar", "foo"]
+
+
+def test_pyarrow_schema():
+    get_pyarrow_schema(MetadataRow, exclude_fields=True)
